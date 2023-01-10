a611d6b721e3bca2caf973359038f1141ec2e0e6
Jeremy Capello <me@lethargicdeveloper.com>
2023-01-09 21:53:09 -0600

    2022.03.2

--- csharp/src/AocLib/Extensions.cs
+++ csharp/src/AocLib/Extensions.cs
@@ -4,4 +4,7 @@ public static class Extensions
 {
     public static void Log(this object obj)
         => Console.WriteLine(obj);
+
+    public static string CreateString(this IEnumerable<char> list)
+        => new(list.ToArray());
 }
---
+++ csharp/src/AocLib/LinqExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace AocLib;
+
+public static class LinqExtensions
+{
+    public static IEnumerable<T> SelectMany<T>(this IEnumerable<IEnumerable<T>> list)
+        => list.SelectMany(_ => _);
+}
