1f4756a01c709dca08b2489eb4501294ada98888
Jeremy Capello <me@lethargicdeveloper.com>
2023-01-09 20:09:45 -0600

    initial commit

---
+++ csharp/src/AocLib/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace AocLib;
+
+public static class Extensions
+{
+    public static string[] SplitLines(this string str)
+        => str.Split(Environment.NewLine);
+
+    public static string[] SplitEmptyLines(this string str)
+        => str.Split($"{Environment.NewLine}{Environment.NewLine}");
+}
