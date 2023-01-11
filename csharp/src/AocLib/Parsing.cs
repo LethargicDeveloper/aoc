22c541fd19a341833a468f6318a7ae98a1b27263
Jeremy Capello <me@lethargicdeveloper.com>
2023-01-10 23:16:58 -0600

    2022.07.1

--- csharp/src/AocLib/Parsing.cs
+++ csharp/src/AocLib/Parsing.cs
@@ -3,10 +3,10 @@
 public static class Parsing
 {
     public static string[] SplitLines(this string str)
-        => str.Split(Environment.NewLine);
+        => str.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
 
     public static string[] SplitEmptyLines(this string str)
-        => str.Split($"{Environment.NewLine}{Environment.NewLine}");
+        => str.Split($"{Environment.NewLine}{Environment.NewLine}", StringSplitOptions.RemoveEmptyEntries);
 
     public static T SplitEmptyLines<T>(this string str, Func<string[], T> func)
         => func(SplitEmptyLines(str));
--- csharp/src/Day07p1/PuzzleSolver.cs
+++ csharp/src/Day07p1/PuzzleSolver.cs
@@ -11,8 +11,50 @@ public partial class PuzzleSolver
     }
 
     [Benchmark]
-    public long Solve()
+    public long Solve() => ParseFileSystem()
+        .Select(_ => _.TotalSize)
+        .Where(size => size <= 100000)
+        .Sum();
+
+    HashSet<Dir> ParseFileSystem() =>
+        input
+            .SplitLines()
+            .Aggregate((fs: new HashSet<Dir>(), wd: (Dir?)null), (acc, cur) =>
+            {
+                var (fs, wd) = acc;
+
+                if (cur.StartsWith("$ cd .."))
+                    return (fs, wd?.Parent);
+
+                if (cur.StartsWith("$ cd "))
+                {
+                    var dir = new Dir
+                    {
+                        Name = cur[5..],
+                        Parent = wd
+                    };
+
+                    fs.Add(dir);
+                    wd?.Children.Add(dir);
+
+                    return (fs, dir);
+                }
+
+                if (long.TryParse(cur.Split(" ")[0], out var size))
+                {
+                    wd!.Size += size;
+                    return (fs, wd);
+                }
+
+                return (fs, wd);
+            }).fs;
+
+    record Dir
     {
-        return 0;
+        public string Name { get; init; } = string.Empty;
+        public long Size { get; set; }
+        public long TotalSize => Size + Children.Sum(_ => _.TotalSize);
+        public Dir? Parent { get; init; } 
+        public List<Dir> Children { get; init; } = new();
     }
 }
