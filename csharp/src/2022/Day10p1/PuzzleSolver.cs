e0bd87d76fb6690f2255f2b534f929bad08deb7c
Jeremy Capello <me@lethargicdeveloper.com>
2023-01-13 21:42:56 -0600

    2022.10.1

---
+++ csharp/src/2022/Day10p1/PuzzleSolver.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+
+[MemoryDiagnoser]
+public partial class PuzzleSolver
+{
+    readonly string input;
+
+    public PuzzleSolver()
+    {
+        this.input = File.ReadAllText("01.txt");
+    }
+
+    [Benchmark]
+    public long Solve()
+    {
+        long x = 1;
+        long cycles = 1;
+        var strengths = new List<long>();
+
+        var cmds = input
+            .SplitLines()
+            .Select(_ => _.Split(' '));
+
+        foreach (var cmd in cmds)
+        {
+            if (++cycles % 40 == 20)
+                strengths.Add(SignalStrength(x, cycles));
+
+            if (cmd[0] == "addx")
+            {
+                x += long.Parse(cmd[1]);
+                if (++cycles % 40 == 20)
+                    strengths.Add(SignalStrength(x, cycles));
+            }
+        }
+
+        return strengths.Sum();
+    }
+
+    long SignalStrength(long x, long cycles) => x * cycles;
+}
