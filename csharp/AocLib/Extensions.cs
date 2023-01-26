1db641534eb101b058fa09e2567abeea545afd0b
Jeremy Capello <me@lethargicdeveloper.com>
2023-01-25 21:53:34 -0600

    2022.22.1 & 2

--- csharp/AocLib/Extensions.cs
+++ csharp/AocLib/Extensions.cs
@@ -1,4 +1,6 @@
-﻿namespace AocLib;
+﻿using System.Numerics;
+
+namespace AocLib;
 
 public static class Extensions
 {
@@ -7,4 +9,11 @@ public static class Extensions
 
     public static string CreateString(this IEnumerable<char> list)
         => new(list.ToArray());
+
+    public static T Mod<T>(this T num, T mod)
+        where T : struct, INumber<T>
+    {
+        var r = num % mod;
+        return r < T.Zero ? r + mod : r;
+    }
 }
