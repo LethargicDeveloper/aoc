152f6cee9cbfac17836e021021dd58309b672d51
Jeremy Capello <me@lethargicdeveloper.com>
2023-01-26 23:33:14 -0600

    2021.11.1-2

--- csharp/2021/src/Day11p2/PuzzleSolver.cs
+++ csharp/2021/src/Day11p2/PuzzleSolver.cs
@@ -1,5 +1,4 @@
 ﻿using BenchmarkDotNet.Attributes;
-using System.Text.RegularExpressions;
 
 [MemoryDiagnoser]
 public partial class PuzzleSolver
@@ -14,95 +13,64 @@ public partial class PuzzleSolver
     [Benchmark]
     public long Solve()
     {
-        var monkeys = this.input
-            .SplitEmptyLines()
-            .Select(Monkey.Parse)
-            .ToList();
+        var grid = input
+           .SplitLines()
+           .Select(_ => _.Select(c => (Value: char.GetNumericValue(c), Flashed: false)).ToArray())
+           .ToArray();
 
-        var testValue = monkeys.Aggregate(1, (acc, cur) => acc * cur.TestValue);
-        for (int i = 0; i < 10000; ++i)
+        int grid_width = grid[0].Length;
+        int grid_height = grid.Length;
+
+        long synched = 0;
+        var flashes = new Stack<Point>();
+        long count = 0;
+        while (synched == 0)
         {
-            foreach (var monkey in monkeys)
+            count++;
+            for (int y = 0; y < grid_height; ++y)
+                for (int x = 0; x < grid_width; ++x)
+                    IncreaseEnergyLevel(x, y);                
+
+            while (flashes.Count > 0)
             {
-                monkey.ThrowItems(monkeys, testValue);
+                var (x, y) = flashes.Pop();
+                if (grid[y][x].Flashed)
+                    continue;
+
+                grid[y][x].Flashed = true;
+
+                IncreaseEnergyLevel(x - 1, y - 1);
+                IncreaseEnergyLevel(x - 1, y);
+                IncreaseEnergyLevel(x - 1, y + 1);
+                IncreaseEnergyLevel(x, y + 1);
+                IncreaseEnergyLevel(x + 1, y + 1);
+                IncreaseEnergyLevel(x + 1, y);
+                IncreaseEnergyLevel(x + 1, y - 1);
+                IncreaseEnergyLevel(x, y - 1);
             }
+
+            for (int y = 0; y < grid_height; ++y)
+                for (int x = 0; x < grid_width; ++x)
+                    if (grid[y][x].Value > 9)
+                    {
+                        grid[y][x].Value = 0;
+                        grid[y][x].Flashed = false;
+                    }
+
+            if (grid.SelectMany(_ => _).All(_ => _.Value == 0))
+                synched = count;
         }
 
-        return monkeys
-            .Select(_ => _.InspectedItems)
-            .OrderDescending()
-            .Take(2)
-            .Product();
-    }
-}
-
-partial class Monkey
-{
-    readonly Queue<long> Items = new();
-    string operand1 = "";
-    string operand2 = "";
-    string @operator = "";
-    int divisor = 1;
-    int monkey1;
-    int monkey2;
-
-    public long InspectedItems { get; private set; }
-    public int TestValue => divisor;
-
-    public void ThrowItems(List<Monkey> monkeys, int testValue)
-    {
-        while (Items.TryDequeue(out long item))
+        void IncreaseEnergyLevel(int x, int y)
         {
-            InspectedItems++;
-            item = CalculateWorryLevel(item);
+            if (x < 0 || y < 0 || x >= grid_width || y >= grid_height)
+                return;
 
-            var monkey = item % divisor == 0 ? monkey1 : monkey2;
-            item %= testValue;
-
-            monkeys[monkey].Items.Enqueue(item);
+            grid[y][x].Value++;
+            if (grid[y][x].Value > 9 && !grid[y][x].Flashed)
+                flashes.Push((x, y));
         }
+
+        return synched;
     }
-
-    long CalculateWorryLevel(long item)
-    {
-        var op1 = operand1 == "old" ? item : long.Parse(operand1);
-        var op2 = operand2 == "old" ? item : long.Parse(operand2);
-        return @operator switch
-        {
-            "+" => op1 + op2,
-            "*" => op1 * op2,
-            _ => throw new InvalidOperationException()
-        };
-    }
-
-    public static Monkey Parse(string input)
-    {
-        var lines = input.Split("\r\n");
-        var id = int.Parse(NumberRegex().Match(lines[0]).Value);
-        var items = NumberRegex().Matches(lines[1]).Select(_ => long.Parse(_.Value));
-        var func = FuncRegex().Matches(lines[2])[0].Groups;
-        var divisor = int.Parse(NumberRegex().Match(lines[3]).Value);
-        var test1 = TestRegex().Matches(lines[4])[0].Groups;
-        var test2 = TestRegex().Matches(lines[5])[0].Groups;
-
-        var monkey = new Monkey();
-        foreach (var item in items)
-            monkey.Items.Enqueue(item);
-        monkey.operand1 = func[1].Value;
-        monkey.@operator = func[2].Value;
-        monkey.operand2 = func[3].Value;
-        monkey.divisor = divisor;
-        monkey.monkey1 = test1[1].Value == "true" ? int.Parse(test1[2].Value) : int.Parse(test2[2].Value);
-        monkey.monkey2 = test2[1].Value == "false" ? int.Parse(test2[2].Value) : int.Parse(test1[2].Value);
-        return monkey;
-    }
-
-    [GeneratedRegex("\\d+")]
-    private static partial Regex NumberRegex();
-
-    [GeneratedRegex("new = (old|\\d+) ([*|+]) (old|\\d+)")]
-    private static partial Regex FuncRegex();
- 
-    [GeneratedRegex("(true|false):.*?(\\d+)")]
-    private static partial Regex TestRegex();
 }
