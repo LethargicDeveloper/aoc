a432604a4cad424c137634b66f0e74a9fcc7d572
Jeremy Capello <me@lethargicdeveloper.com>
2023-12-04 00:35:35 -0600

    2023.4

--- src/AdventOfCode/Program.cs
+++ src/AdventOfCode/Program.cs
@@ -10,7 +10,7 @@
  *  
  */
 
-var puzzle = new AdventOfCode._2023.Day03.Part02();
+var puzzle = new AdventOfCode._2023.Day04.Part02();
 puzzle.Solve().Log();
 
 // BenchmarkDotNet.Running.BenchmarkRunner.Run<AdventOfCode._2023.Day06.Part02>();
