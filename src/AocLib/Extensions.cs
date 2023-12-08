d7260e2bfe0c169b1048d160390651c17c4417d0
Jeremy Capello <me@lethargicdeveloper.com>
2023-12-08 01:07:17 -0600

    2023.8

--- src/AocLib/Extensions.cs
+++ src/AocLib/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using static QuikGraph.Algorithms.Assignment.HungarianAlgorithm;
 
 namespace AocLib;
 
---
+++ src/AocLib/MathEx.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace AocLib;
+
+public static class MathEx
+{
+    public static long LCM(long a, long b)
+        => (a / GCF(a, b)) * b;
+
+    public static long GCF(long a, long b)
+    {
+        while (b != 0)
+        {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
+
+public static class NumericExtensions
+{
+    public static long LCM(this long[] longs)
+    {
+        long acc = longs[0];
+        for (int i = 1; i < longs.Length; ++i)
+        {
+            acc = MathEx.LCM(acc, longs[i]);
+        }
+
+        return acc;
+    }
+}
