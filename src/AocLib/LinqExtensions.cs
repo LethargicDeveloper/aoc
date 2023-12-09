d3ed0fc799ffe48d419463cb8dcf99f7dc7d56c8
Jeremy Capello <me@lethargicdeveloper.com>
2023-12-08 22:00:48 -0600

    removed unused code

--- src/AocLib/LinqExtensions.cs
+++ src/AocLib/LinqExtensions.cs
@@ -5,15 +5,6 @@ namespace AocLib;
 
 public static class LinqExtensions
 {
-    public static IEnumerable<T> Select<T>(this IEnumerable<T> list)
-        => list.Select(_ => _);
-
-    public static IEnumerable<T> SelectMany<T>(this IEnumerable<IEnumerable<T>> list)
-        => list.SelectMany(_ => _);
-
-    public static IEnumerable<IGrouping<T, T>> GroupBy<T>(this IEnumerable<T> list)
-        => list.GroupBy(_ => _);
-
     public static IEnumerable<(int Index, T Value)> WithIndex<T>(this IEnumerable<T> list)
         => list.Select((v, i) => (i, v));
 
