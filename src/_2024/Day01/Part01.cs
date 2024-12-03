d42daa49f92c633dc0f36c525336b7ff0f79737a
Jeremy Capello <me@lethargicdeveloper.com>
2024-12-03 08:58:33 -0600

    2024.01 performance

--- src/_2024/Day01/Part01.cs
+++ src/_2024/Day01/Part01.cs
@@ -1,28 +1,46 @@
 using AocLib;
-using MoreLinq;
+
+using System.Globalization;
+
+
 
 namespace _2024.Day01;
 
-public class Part01 : PuzzleSolver<long>
+public class Part01 : PuzzleSolver<int>
 {
-    protected override long InternalSolve()
-    {
-        var (list1, list2) = input
-            .SplitLines()
-            .Select(x => x.Split("   "))
-            .Aggregate((List1: new List<long>(), List2: new List<long>()), (acc, cur) =>
-            {
-                acc.List1.Add(int.Parse(cur[0]));
-                acc.List2.Add(int.Parse(cur[1]));
-                return acc;
-            });
+    private const int LINE_COUNT = 1000;
 
-        var answer = list1
-            .OrderBy()
-            .Zip(list2.OrderBy(),
-                (a, b) => Math.Abs(a - b))
-            .Sum();
+    protected override int InternalSolve()
+    {
+        var lines = input.AsSpan();
         
-        return answer;
+        var list = new int[LINE_COUNT * 2];
+        
+        for (int i = 0, j = 0; i < input.Length; i += 14, j++)
+        {
+            list[j] = FastParse(lines[i..(i + 5)]);
+            list[LINE_COUNT + j] = FastParse(lines[(i + 8)..(i + 13)]);
+        }
+        
+        Array.Sort(list, 0, LINE_COUNT);
+        Array.Sort(list, LINE_COUNT, LINE_COUNT);
+        
+        var total = 0;
+        
+        for (int i = 0; i < LINE_COUNT; i++)
+        {
+            total += Math.Abs(list[i] - list[LINE_COUNT + i]);
+        }
+
+        return total;
+    }
+    
+    private static int FastParse(ReadOnlySpan<char> input)
+    {
+        return (input[0] - '0') * 10000 +
+               (input[1] - '0') * 1000 +
+               (input[2] - '0') * 100 +
+               (input[3] - '0') * 10 +
+               (input[4] - '0');
     }
 }
