520c5a3a5f718918ac8443c906e366e9fb9ea8a4
Jeremy Capello <me@lethargicdeveloper.com>
2025-12-02 11:44:03 -0600

    2025.2 smaller solution

--- src/_2025/Day02/Part02.cs
+++ src/_2025/Day02/Part02.cs
@@ -9,45 +9,46 @@ public class Part02 : PuzzleSolver<long>
         return input
             .S(',')
             .Select(x => x.Split('-'))
-            .SelectMany(ValidIds)
+            .SelectMany(x => new AocRange<long>(long.Parse(x[0]), long.Parse(x[1]))
+                .Where(a => $"{a}{a}"[1..^1].Contains($"{a}")))
             .Sum();
     }
 
-    IEnumerable<long> ValidIds(string[] ids)
-    {
-        var start = long.Parse(ids[0]);
-        var end = long.Parse(ids[1]);
-
-        return new AocRange<long>(start, end)
-            .Select(x => x.ToString())
-            .Where(id => id[0] == '0' || HasRepeatingPattern(id))
-            .Select(long.Parse);
-    }
-
-    bool HasRepeatingPattern(string id)
-    {
-        for (var i = 0; i < id.Length / 2; i++)
-        {
-            if (HasRepeatingPattern(id, i + 1))
-                return true;
-        }
-        
-        return false;
-    }
-    
-    bool HasRepeatingPattern(string id, int offset)
-    {
-        if (id.Length % offset != 0)
-            return false;
-        
-        var pattern = id[..offset];
-        
-        for (int i = offset; i <= id.Length - offset; i += offset)
-        {
-            var ix = id[i..(i + offset)];
-            if (ix != pattern) return false;
-        }
-
-        return true;
-    }
+    // IEnumerable<long> ValidIds(string[] ids)
+    // {
+    //     var start = long.Parse(ids[0]);
+    //     var end = long.Parse(ids[1]);
+    //
+    //     return new AocRange<long>(start, end)
+    //         .Select(x => x.ToString())
+    //         .Where(id => id[0] == '0' || HasRepeatingPattern(id))
+    //         .Select(long.Parse);
+    // }
+    //
+    // bool HasRepeatingPattern(string id)
+    // {
+    //     for (var i = 0; i < id.Length / 2; i++)
+    //     {
+    //         if (HasRepeatingPattern(id, i + 1))
+    //             return true;
+    //     }
+    //     
+    //     return false;
+    // }
+    //
+    // bool HasRepeatingPattern(string id, int offset)
+    // {
+    //     if (id.Length % offset != 0)
+    //         return false;
+    //     
+    //     var pattern = id[..offset];
+    //     
+    //     for (int i = offset; i <= id.Length - offset; i += offset)
+    //     {
+    //         var ix = id[i..(i + offset)];
+    //         if (ix != pattern) return false;
+    //     }
+    //
+    //     return true;
+    // }
 }
